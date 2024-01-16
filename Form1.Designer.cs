a9a86500245d1476efd9c99b0724c654c42a2123
thebestofbwgkl@outlook.com <thebestofbwgkl@outlook.com>
2024-01-16 15:02:10 +0500

    Только элементы(доделать и дальше)

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblLog = new System.Windows.Forms.Label();
+            this.lblPass = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // lblLog
+            // 
+            this.lblLog.AutoSize = true;
+            this.lblLog.Location = new System.Drawing.Point(46, 50);
+            this.lblLog.Name = "lblLog";
+            this.lblLog.Size = new System.Drawing.Size(38, 13);
+            this.lblLog.TabIndex = 0;
+            this.lblLog.Text = "Логин";
+            // 
+            // lblPass
+            // 
+            this.lblPass.AutoSize = true;
+            this.lblPass.Location = new System.Drawing.Point(46, 124);
+            this.lblPass.Name = "lblPass";
+            this.lblPass.Size = new System.Drawing.Size(45, 13);
+            this.lblPass.TabIndex = 0;
+            this.lblPass.Text = "Пароль";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(211, 123);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(114, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Показать пароль";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(50, 82);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(161, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(49, 184);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(161, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(42, 255);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 54);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Войти";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(316, 255);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 54);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Отмена";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 439);
+            this.ClientSize = new System.Drawing.Size(519, 359);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.lblPass);
+            this.Controls.Add(this.lblLog);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblLog;
+        private System.Windows.Forms.Label lblPass;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
